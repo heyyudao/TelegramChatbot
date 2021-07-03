16f0af98e49dccf2d461658d6673608ebebf456a
heyyudao <62549753+heyyudao@users.noreply.github.com>
2021-07-03 12:04:33 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,31 @@
+from bot import telegram_chatbot
+import gizoogle
+
+bot = telegram_chatbot("config.cfg")
+
+
+def make_reply(msg):
+    reply = None
+    if msg is not None:
+        reply  =  gizoogle.text(msg) # 'bot is working hard'#
+    return reply
+
+update_id = None
+while True:
+    updates = bot.get_updates(offset=update_id)
+    
+    updates = updates["result"]
+    
+    if updates:
+        for item in updates:
+            
+            update_id = item["update_id"]
+            
+            try:
+                message = str(item["message"]["text"])
+                print(message)
+            except:
+                message = None
+            from_ = item["message"]["from"]["id"]
+            reply = make_reply(message)
+            bot.send_message(reply, from_)
